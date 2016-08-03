61b1923e7a2a82bca870c6ee0ad04b39baa906ef
Peter Mitrano <mitranopeter@gmail.com>
2016-08-03 01:05:53 -0400

    actually scan db every second watch_local script

--- test/watch_local_db.py
+++ test/watch_local_db.py
@@ -8,11 +8,12 @@ from time import sleep
 
 if __name__ == "__main__":
     db = boto3.client('dynamodb', endpoint_url="http://localhost:8000")
-    db.list_tables()
-    table = db.scan(TableName = "my_cookbook_users")
 
     while True:
         sleep(1)
+
+        table = db.scan(TableName = "my_cookbook_users")
+
         for item in table['Items']:
             userId = item['userId']['S']
             mapAttr = item['mapAttr']['M']
