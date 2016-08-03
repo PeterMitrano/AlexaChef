e611a54bfb5282aae1b84e7b485515d3e5204933
Peter Mitrano <mitranopeter@gmail.com>
2016-08-03 02:12:31 -0400

    moved things to util folder
    also I was wrong about mocha using some port. but I don't feel like
    changing it back. Mocha was accidentally running server.js

---
+++ test/tests.js
@@ -0,0 +1,11 @@
+'use strict';
+
+//let index = require("../src/index");
+//let generator = require("./generate_utterances");
+
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Launch', function() {
+  it('should work from all states');
+});
