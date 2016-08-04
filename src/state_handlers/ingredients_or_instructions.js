ecb2e8bb7b32e5371057c019cfd2c0435dca9467
Peter Mitrano <mitranopeter@gmail.com>
2016-08-04 03:02:49 -0400

    tested cards. they work

--- src/state_handlers/ingredients_or_instructions.js
+++ src/state_handlers/ingredients_or_instructions.js
@@ -14,10 +14,10 @@ var Alexa = require('../alexa');
  */
 module.exports = Alexa.CreateStateHandler(Core.states.INGREDIENTS_OR_INSTRUCTIONS, {
   'IngredientsIntent': function() {
-    this.emit(":ask", "Starting with ingredients");
+    this.emit(":tellWithCard", "Here are the ingredients", "Ingredients", " - eggs\n - milk\n");
   },
   'InstructionsIntent': function() {
-    this.emit(":ask", "Starting with instructions");
+    this.emit(":tellWithCard", "Here are the instructions", "Instrcutions", " - mix eggs and milk\n - throw it on the ground\n -profit");
   },
   'Unhandled': function() {
     this.emit(":ask", "I'm confused. Do you want to start with ingredients or instructions?");
