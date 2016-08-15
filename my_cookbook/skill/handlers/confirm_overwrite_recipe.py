a3782ccafd4525f796ff138cf5168a4715d0458a
Peter Mitrano <mitranopeter@gmail.com>
2016-08-15 03:50:29 -0400

    fixing up save recipe flow
    still not eonugh tests

--- my_cookbook/skill/handlers/confirm_overwrite_recipe.py
+++ my_cookbook/skill/handlers/confirm_overwrite_recipe.py
@@ -3,7 +3,7 @@ from my_cookbook.util import responder
 
 class ConfirmOverwriteRecipe():
     def AMAZON_YesIntent(self, handlers, persistant_attributes, attributes, slots):
-        if 'current_recipe' not in attributes:
+        if 'current_recipe' not in attributes or 'tmp_state' not in attributes:
             return responder.tell("Sorry, but I've forgotten which recipe you wanted to save. You'll have to start over.")
 
         current_recipe = attributes['current_recipe']
@@ -12,13 +12,18 @@ class ConfirmOverwriteRecipe():
         return responder.tell('Recipe overwritten')
 
     def AMAZON_NoIntent(self, handlers, persistant_attributes, attributes, slots):
-        persistant_attributes[core.STATE_KEY] = attributes['tmp_state']
+        #TODO: consider how we might recover/fail if this isn't the case
+        if 'tmp_state' in attributes:
+            persistant_attributes[core.STATE_KEY] = attributes['tmp_state']
+
         return responder.tell('Cancelling overwrite')
 
     def Unhandled(self, handlers, persistant_attributes, attributes, slots):
         persistant_attributes[core.STATE_KEY] = attributes[core.STATE_KEY]
         return responder.ask("I'm not sure what you mean. \
-                Do you want to overwrite the existing recipe? Say yes or no")
+                Do you want to overwrite the existing recipe? Say yes or no",
+                None,
+                attributes)
 
 handler = ConfirmOverwriteRecipe()
 state = core.States.CONFIRM_OVERWRITE_RECIPE
