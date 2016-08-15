62ec2f7620920c9810b65f2a531dfc756da2b89a
Peter Mitrano <mitranopeter@gmail.com>
2016-08-14 22:01:54 -0400

    checking in speech stuff

---
+++ interaction_model/intent_schema.py
@@ -0,0 +1,40 @@
+{
+    "intents": [
+        {
+            "intent": "StartNewRecipeIntent",
+            "slots": [
+                {
+                    "name": "RecipeName",
+                    "type": "AMAZON.LITERAL"
+                }
+            ]
+        },
+        {
+            "intent": "IngredientsIntent"
+        },
+        {
+            "intent": "InstructionsIntent"
+        },
+        {
+            "intent": "AMAZON.HelpIntent"
+        },
+        {
+            "intent": "AMAZON.NextIntent"
+        },
+        {
+            "intent": "AMAZON.PreviousIntent"
+        },
+        {
+            "intent": "AMAZON.RepeatIntent"
+        },
+        {
+            "intent": "AMAZON.StartOverIntent"
+        },
+        {
+            "intent": "AMAZON.YesIntent"
+        },
+        {
+            "intent": "AMAZON.NoIntent"
+        }
+    ]
+}
