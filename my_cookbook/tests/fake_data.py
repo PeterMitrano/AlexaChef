1247005ed309099e77a40af24ba801ed222cd048
Peter Mitrano <mitranopeter@gmail.com>
2016-08-19 17:27:17 -0400

    simplified fake data
    using Title instead of Name to match bigoven

--- my_cookbook/tests/fake_data.py
+++ my_cookbook/tests/fake_data.py
@@ -1,97 +1,10 @@
-pancakes = {
-    'id': 0,
-    "category": {
-        "name": "breakfast",
-        "subcategory": {
-            "attributes": "buttermilk",
-            "name": "pancakes"
-        }
-    },
-    "cook_time": "10 minutes",
-    "ingredients": [
-        {
-            "name": "flour"
-        }, {
-            "name": "salt"
-        }, {
-            "name": "baking soda"
-        }, {
-            "name": "baking powder"
-        }, {
-            "name": "egg"
-        }, {
-            "name": "buttermilk"
-        }, {
-            "name": "butter"
-        }
-    ],
-    "instructions": [
-        {
-            "speech": [
-                "mix", {
-                    "name": "flour",
-                    "quantity": 0.5,
-                    "units": "cups"
-                }, "with", {
-                    "name": "salt",
-                    "quantity": 0.25,
-                    "units": "teaspoon"
-                }, ",", {
-                    "name": "baking soda",
-                    "quantity": 0.25,
-                    "units": "teaspoon"
-                }, ", and", {
-                    "name": "baking powder",
-                    "quantity": 0.5,
-                    "units": "teaspoon"
-                }
-            ]
-        }, {
-            "speech": [
-                "beat in an", {
-                    "name": "egg",
-                    "quantity": 1,
-                    "units": "None"
-                }, "and", {
-                    "name": "buttermilk",
-                    "quantity": 0.5,
-                    "units": "cups"
-                }
-            ]
-        }, {
-            "speech": [
-                "melt the", {
-                    "name": "butter",
-                    "quantity": 1,
-                    "units": "tablespoon"
-                }, "and beat into the batter"
-            ]
-        }
-    ],
-    "main_ingredient": "buttermilk",
-    "name": "classic buttermilk pancakes",
-    "nutrition": {
-        "fat": {
-            "unit": "g",
-            "value": 2
-        },
-        "sodium": {
-            "unit": "g",
-            "value": 1
-        }
-    },
-    "prep_time": "10 minutes",
-    "tags": [
-        "sweet", "buttery", "warm"
-    ],
-    "total_time": "20 minutes"
-}
-pancakes2 = {"id": 1, "name": "best pancakes ever", "instructions": [], "ingredients": []}
-pancakes3 = {"id": 2, "name": "real shitty pancakes", "instructions": [], "ingredients": []}
-biscuits = {"id": 3, "name": "southern buscuits", "instructions": [], "ingredients": []}
-biscuits2 = {"id": 4, "name": "non-existant buscuits", "instructions": [], "ingredients": []}
-chicken_pot_pie = {"id": 5, "name": "hearthy chicken pot pie", "instructions": [], "ingredients": []}
-pizza= {"id": 6, "name": "pizza", "instructions": [], "ingredients": []}
+pancakes = { 'id': 0, "Title": "classic buttermilk pancakes", "Ingredients": [], "Instructions": []}
+pancakes2 = {"id": 1, "Title": "best pancakes ever", "Instructions": [], "Ingredients": []}
+pancakes3 = {"id": 2, "Title": "real shitty pancakes", "Instructions": [], "Ingredients": []}
+biscuits = {"id": 3, "Title": "southern buscuits", "Instructions": [], "Ingredients": []}
+biscuits2 = {"id": 4, "Title": "non-existant buscuits", "Instructions": [], "Ingredients": []}
+chicken_pot_pie = {"id": 5, "Title": "hearthy chicken pot pie", "Instructions": [], "Ingredients": []}
+pizza= {"id": 6, "Title": "pizza", "Instructions": [], "Ingredients": []}
 
 test_online_recipes = [
     pancakes,
