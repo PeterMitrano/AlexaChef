be54ed77f3690a14ca23ccc2938586eee9da833c
Peter Mitrano <mitranopeter@gmail.com>
2016-08-19 17:28:46 -0400

    update recipe reader
    we are working under the assumption that Instructions is a python list
    of speech-ready test. I'm going to handle that assumption in the
    recipe-retrieval part. Of course bigoven recipes aren't that way, so I
    need to figure out how to make this happen, but the point is I don't
    want that issue to infect any other part of the code. It should only be
    dealt with in reacipes_helper.py

--- my_cookbook/util/recipe_reader.py
+++ my_cookbook/util/recipe_reader.py
@@ -4,8 +4,28 @@ from my_cookbook.util import core
 ingredient_pause_time = 1.3
 
 
+def ingredients(attributes):
+    speech = ingredients_speech(attributes['current_recipe'])
+    card = ingredients_card(attributes['current_recipe'])
+    return responder.ask_with_card("The ingredients are. " + speech, None, "Ingredients", card,
+                                   None, attributes)
+
+def ingredients_speech(recipe):
+    """ return ingredients ready for speech
+
+    uses custom break to seperate each ingredient """
+
+    ingredients = [i['Name'] for i in recipe['Ingredients']]
+    separator = '<break time="%ss"/>' % ingredient_pause_time
+    return separator.join(ingredients)
+
+def ingredients_card(recipe):
+    ingredients = [i['Name'] for i in recipe['Ingredients']]
+    separator = '\n - '
+    return separator.join(ingredients)
+
 def instruct(step_number, attributes):
-    instructions = attributes['current_recipe']['instructions']
+    instructions = attributes['current_recipe']['Instructions']
     if len(instructions) <= step_number:
         return responder.tell("this recipe doesn't have any instructions.")
 
@@ -16,31 +36,6 @@ def instruct(step_number, attributes):
         instruction + ". <break time=2/> would you like to hear the next step?", None,
         "Instructions", card, None, attributes)
 
-
-def ingredients(attributes):
-    speech = ingredients_speech(attributes['current_recipe'])
-    card = ingredients_card(attributes['current_recipe'])
-    return responder.ask_with_card("The ingredients are. " + speech, None, "Ingredients", card,
-                                   None, attributes)
-
-
-def ingredients_speech(recipe):
-    """ return ingredients ready for speech
-
-    uses custom break to seperate each ingredient """
-
-    ingredients = [i['name'] for i in recipe['ingredients']]
-    separator = '<break time="%ss"/>' % ingredient_pause_time
-    return separator.join(ingredients)
-
-
-def ingredients_card(recipe):
-    ingredients = [i['name'] for i in recipe['ingredients']]
-    separator = '\n - '
-    return separator.join(ingredients)
-
-
-def instructions_card(recipe):
-    instructions = [i['name'] for i in recipe['instructions']]
+def instructions_card(instructions):
     separator = '\n - '
     return separator.join(instructions)
